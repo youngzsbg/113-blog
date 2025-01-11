ce5daa929c88772a0cb00f67463be049b2090545
Kendall Payne <k-payne101@hotmail.com>
2025-01-11 00:43:37 -0500

    First commit

---
+++ posts/views.py
@@ -0,0 +1,79 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post, Status
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import (
+     LoginRequiredMixin,
+     UserPassesTestMixin
+)
+
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        published = Status.objects.get(name="published")
+        context["title"] = "Published"
+        context["post_list"] =(
+            Post.objects
+            .filter(status=published)
+            .order_by("created_on").reverse()
+        )
+        return context
+    
+class DraftPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft = Status.objects.get(name="draft")
+        context["title"] = "Draft"
+        context["post_list"] = (
+            Post.objects
+            .filter(status=draft)
+            .filter(author=self.request.user)
+            .order_by("created_on").reverse()
+        )
+        return context
+
+class PostDetailView(DetailView):
+        template_name = "posts/detail.html"
+        model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+        
+
